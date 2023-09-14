b5ab48068265761e65d30593cfefef13d30b558c
Jonathan-Rod <jonathan.rodriguez72@upr.edu>
2023-09-13 22:41:37 -0400

    Added the function getRatingEmoji

--- TutoringApp/components/atoms/tutor.jsx
+++ TutoringApp/components/atoms/tutor.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+function getRatingEmoji(rating) {
+  const boundedRate = rating < 0 ? 0 : rating > 5 ? 5 : rating;
+  const maxRating = "⭐⭐⭐⭐⭐";
+  return maxRating.substring(0, boundedRate);
+}
 
 const Tutor = ({ name, course, rating }) => (
   <View style={styles.container}>
     <Text style={styles.name}>{name}</Text>
     <Text style={styles.course}>{course}</Text>
-    <Text style={styles.rating}>{rating}</Text>
+    <Text style={styles.rating}>{getRatingEmoji(rating)}</Text>
   </View>
 );
 
