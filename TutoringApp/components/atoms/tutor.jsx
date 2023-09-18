da211c00b221fee24d69c46333de356d3bd12e68
Jonathan-Rod <jonathan.rodriguez72@upr.edu>
2023-09-17 20:58:15 -0400

    Refactored the code to implement 'react native paper'

--- TutoringApp/components/atoms/tutor.jsx
+++ TutoringApp/components/atoms/tutor.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, Card } from "react-native-paper";
 
 function getRatingEmoji(rating) {
   const maxRating = "⭐⭐⭐⭐⭐";
@@ -15,31 +15,15 @@ function getRatingEmoji(rating) {
 }
 
 const Tutor = ({ name, course, rating }) => (
-  <View style={styles.container}>
-    <Text style={styles.name}>{name}</Text>
-    <Text style={styles.course}>{course}</Text>
-    <Text style={styles.rating}>{getRatingEmoji(rating)}</Text>
-  </View>
+  <Card>
+    <Card.Content>
+      <Text>{name}</Text>
+      <Text>{course}</Text>
+    </Card.Content>
+    <Card.Actions>
+      <Text>{getRatingEmoji(rating)}</Text>
+    </Card.Actions>
+  </Card>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  name: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  course: {
-    fontSize: 16,
-  },
-  rating: {
-    fontSize: 14,
-    fontStyle: "italic",
-  },
-});
 
 export default Tutor;
