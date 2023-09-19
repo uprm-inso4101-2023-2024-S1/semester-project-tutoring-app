e0a8843d80dd4fb2b6717968b6634bd403380c50
Yayo <yahid.diaz@upr.edu>
2023-09-19 15:42:15 -0400

    Deleting useless stuff

--- TutoringApp/components/pages/services.js
+++ TutoringApp/components/pages/services.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, StyleSheet, ScrollView } from "react-native";
+
 import MySearchBar from "../atoms/my-search-bar";
 
 
@@ -15,49 +15,4 @@ const Services = () => {
     </>
   );
 };
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  profile: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    padding: 16,
-  },
-  overlapContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 1,
-  },
-  row: {
-    flexDirection: "row", 
-    alignItems: "center", 
-    padding: 16,
-  },
-  item: {
-    backgroundColor: "lightgray",
-    padding: 10,
-    marginVertical: 5,
-  },
-  textbox: {
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 15,
-    margin: 5,
-  },
-  button: {
-    backgroundColor: "lightblue",
-    padding: 10,
-    borderRadius: 5,
-    margin: 3,
-  },
-});
-
 export default Services;
