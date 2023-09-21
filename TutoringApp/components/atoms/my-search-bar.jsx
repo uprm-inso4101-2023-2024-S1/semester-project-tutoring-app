fc011e9b7432bd3a8277b64c787a4a0656b4cb26
William A. Martínez Martínez <william.martinez10@upr.edu>
2023-09-21 17:41:05 -0400

    added component and simple styling for search bar

--- TutoringApp/components/atoms/my-search-bar.jsx
+++ TutoringApp/components/atoms/my-search-bar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Surface, Searchbar } from "react-native-paper";
 import DepartmentComponent from "./DepartmentComponent";
+import { StyleSheet } from "react-native";
+import NoResultsSearch from "./NoResultsSearch";
 
 const MySearchBar = ({ contentList }) => {
   let [searchText, setSearchText] = useState("");
@@ -32,7 +34,7 @@ const MySearchBar = ({ contentList }) => {
   };
 
   return (
-    <Surface>
+    <Surface style={styles.surface}>
       <Searchbar
         placeholder="Search..."
         onChangeText={(text) => {
@@ -42,15 +44,26 @@ const MySearchBar = ({ contentList }) => {
         value={searchText}
       />
 
-      {searchResults.map((result, index) => (
-        <DepartmentComponent
-          key={index}
-          departmentName={result.name}
-          courseData={result.courseData}
-        />
-      ))}
+      {searchResults.length === 0 ? (
+        <NoResultsSearch />
+      ) : (
+        searchResults.map((result, index) => (
+          <DepartmentComponent
+            key={index}
+            departmentName={result.name}
+            courseData={result.courseData}
+          />
+        ))
+      )}
     </Surface>
   );
 };
+
+const styles = StyleSheet.create({
+  surface: {
+    flex: 1,
+    padding: 16,
+  },
+});
 
 export default MySearchBar;
