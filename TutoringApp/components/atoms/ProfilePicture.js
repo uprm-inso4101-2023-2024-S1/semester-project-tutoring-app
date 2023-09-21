28b42c7f6fe7ee30f6bf5fa0ef84935a856de785
Jose Alejandro <jacorderopr@gmail.com>
2023-09-21 16:46:39 -0400

    created a profilePicture Component that takes in a path to an image within the project and displays it in a circular manner. It also contains a gray border that surrounds said image

---
+++ TutoringApp/components/atoms/ProfilePicture.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const ProfilePicture = ({ imagePath }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        // source={{uri : (imagePath)}} // You can use 'require' for local images
+        source={imagePath}
+        style={styles.image}
+        resizeMode="cover" // You can change resizeMode to fit your UI requirements
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 200, // Adjust the width and height as per your UI design
+    height: 200,
+    borderRadius: 100, // To make it a circular profile picture
+    overflow: 'hidden', // Clip the image to the borderRadius
+    borderWidth: 5,
+    borderColor : 'gray'
+  },
+  image: {
+    flex: 1, // Make the image expand to fill the container
+    width: null, // Reset width and height to make it responsive
+    height: null,
+  },
+});
+
+export default ProfilePicture;
