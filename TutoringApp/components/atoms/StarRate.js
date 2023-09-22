29f4876876065b6e2984e0371fed9d3058fd978d
WilliamRiveraMoret <William.rivera60@upr.edu>
2023-09-22 14:55:28 -0400

    Initial Recommeded Tutor Card

---
+++ TutoringApp/components/atoms/StarRate.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+export default function StarRating({ rating }) {
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            {[...Array(5)].map((_, index) => {
+                const star_icon = Math.round(rating) <= index
+                    ? require("../../assets/star_icon_black.png")
+                    : require("../../assets/star_icon_yellow.png");
+                return (
+                    <Image
+                        key={index}
+                        source={star_icon}
+                        aria-label='star'
+                        style={{
+                            width: 25,
+                            height: 25,
+                        }}
+                    />
+                );
+            })}
+        </View>
+    );
+}
