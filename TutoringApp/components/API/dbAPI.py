47fbdd7d1621ff72dba887c0985df12e453f584e
Draygon1016 <joshua.rosado7@upr.edu>
2023-09-25 09:18:42 -0400

    Added Put and Delete functions

--- TutoringApp/components/API/dbAPI.py
+++ TutoringApp/components/API/dbAPI.py
@@ -24,18 +24,20 @@ async def DataBasePost(table_name, data):
         else:
             return False
 
-# async def DataBasePut(table_name, data):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.patch(f'{DB_URL}/{table_name}', headers={'apikey': API_KEY}, json=data)
-#         if response.status_code == 200:
-#             return True
-#         else:
-#             return False
+async def DataBasePut(table_name, data):
+    #needs more testing
+    async with httpx.AsyncClient() as client:
+        response = await client.patch(f'{DB_URL}/{table_name}', headers={'apikey': API_KEY}, json=data)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
 
-# async def DataBaseDelete(table_name, data):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.delete(f'{DB_URL}/{table_name}', headers={'apikey': API_KEY}, json=data)
-#         return response
+async def DataBaseDelete(table_name, data):
+    #needs more testing
+    async with httpx.AsyncClient() as client:
+        response = await client.delete(f'{DB_URL}/{table_name}', headers={'apikey': API_KEY}, json=data)
+        return response
 
 @app.route('/users', methods=['GET'])
 async def GetUsers():
@@ -58,26 +60,28 @@ async def CreateUser():
         return jsonify({'message': 'Error creating user'}), 500
 
 
-# @app.route('/users', methods=['PUT'])
-# async def update_user():
-#     data = request.get_json()
-#     success = await DataBasePut('users', data)
-#     if success:
-#         return jsonify({'message': 'User updated successfully'}), 200
-#     else:
-#         return jsonify({'message': 'Error updating user'}), 500
+@app.route('/users', methods=['PUT'])
+async def update_user():
+    #needs more testing
+    data = request.get_json()
+    success = await DataBasePut('users', data)
+    if success:
+        return jsonify({'message': 'User updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Error updating user'}), 500
 
 
-# @app.route('/28577', methods=['DELETE'])
-# async def delete_user():
-#     data = request.get_json()
-#     if 'user_id' not in data:
-#         return jsonify({'message': 'user_id is required'}), 400
-#     response = await DataBaseDelete('users', data)
-#     if response.status_code == 204:
-#         return jsonify({'message': 'User deleted successfully'}), 204
-#     else:
-#         return jsonify({'message': 'Error deleting user'}), response.status_code
+@app.route('/28577', methods=['DELETE'])
+async def delete_user():
+    #needs more testing
+    data = request.get_json()
+    if 'user_id' not in data:
+        return jsonify({'message': 'user_id is required'}), 400
+    response = await DataBaseDelete('users', data)
+    if response.status_code == 204:
+        return jsonify({'message': 'User deleted successfully'}), 204
+    else:
+        return jsonify({'message': 'Error deleting user'}), response.status_code
 
 # Courses
 
@@ -100,27 +104,29 @@ async def CreateCourses():
     else:
         return jsonify({'message': 'Error creating course'}), 500
 
-# @app.route('/courses', methods=['PUT'])
-# async def update_course():
-#     data = request.get_json()
-#     if 'course_id' not in data or 'course_name' not in data or "description" not in data:
-#         return jsonify({'message': 'course_id, course_name, and description are required'}), 400
-#     success = await DataBasePut('courses', data)
-#     if success:
-#         return jsonify({'message': 'Course updated successfully'}), 200
-#     else:
-#         return jsonify({'message': 'Error updating course'}), 500
+@app.route('/courses', methods=['PUT'])
+async def update_course():
+    #needs more testing
+    data = request.get_json()
+    if 'course_id' not in data or 'course_name' not in data or "description" not in data:
+        return jsonify({'message': 'course_id, course_name, and description are required'}), 400
+    success = await DataBasePut('courses', data)
+    if success:
+        return jsonify({'message': 'Course updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Error updating course'}), 500
 
-# @app.route('/courses', methods=['DELETE'])
-# async def delete_course():
-#     data = request.get_json()
-#     if 'course_id' not in data:
-#         return jsonify({'message': 'course_id is required'}), 400
-#     response = await DataBaseDelete('courses', data)
-#     if response.status_code == 204:
-#         return jsonify({'message': 'Course deleted successfully'}), 204
-#     else:
-#         return jsonify({'message': 'Error deleting course'}), response.status_code
+@app.route('/courses', methods=['DELETE'])
+async def delete_course():
+    #needs more testing
+    data = request.get_json()
+    if 'course_id' not in data:
+        return jsonify({'message': 'course_id is required'}), 400
+    response = await DataBaseDelete('courses', data)
+    if response.status_code == 204:
+        return jsonify({'message': 'Course deleted successfully'}), 204
+    else:
+        return jsonify({'message': 'Error deleting course'}), response.status_code
 
 # # Course Schedules
 
@@ -143,27 +149,29 @@ async def create_course_schedule():
     else:
         return jsonify({'message': 'Error creating course schedule'}), 500
 
-# @app.route('/classschedules', methods=['PUT'])
-# async def update_course_schedule():
-#     data = request.get_json()
-#     if 'schedule_id' not in data or 'start_time' not in data or 'end_time' not in data:
-#         return jsonify({'message': 'schedule_id, start_time, and end_time are required'}), 400
-#     success = await DataBasePut('classschedules', data)
-#     if success:
-#         return jsonify({'message': 'Course schedule updated successfully'}), 200
-#     else:
-#         return jsonify({'message': 'Error updating course schedule'}), 500
+@app.route('/classschedules', methods=['PUT'])
+async def update_course_schedule():
+    #needs more testing
+    data = request.get_json()
+    if 'schedule_id' not in data or 'start_time' not in data or 'end_time' not in data:
+        return jsonify({'message': 'schedule_id, start_time, and end_time are required'}), 400
+    success = await DataBasePut('classschedules', data)
+    if success:
+        return jsonify({'message': 'Course schedule updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Error updating course schedule'}), 500
 
-# @app.route('/classschedules', methods=['DELETE'])
-# async def delete_course_schedule():
-#     data = request.get_json()
-#     if 'schedule_id' not in data:
-#         return jsonify({'message': 'schedule_id is required'}), 400
-#     response = await DataBaseDelete('classschedules', data)
-#     if response.status_code == 204:
-#         return jsonify({'message': 'Course schedule deleted successfully'}), 204
-#     else:
-#         return jsonify({'message': 'Error deleting course schedule'}), response.status_code
+@app.route('/classschedules', methods=['DELETE'])
+async def delete_course_schedule():
+    #needs more testing
+    data = request.get_json()
+    if 'schedule_id' not in data:
+        return jsonify({'message': 'schedule_id is required'}), 400
+    response = await DataBaseDelete('classschedules', data)
+    if response.status_code == 204:
+        return jsonify({'message': 'Course schedule deleted successfully'}), 204
+    else:
+        return jsonify({'message': 'Error deleting course schedule'}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
