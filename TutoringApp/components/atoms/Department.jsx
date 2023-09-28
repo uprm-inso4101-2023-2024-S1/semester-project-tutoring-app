4c23cc07b6ccdc34a45b917dcf0e8e616ecbd516
Jonathan-Rod <jonathan.rodriguez72@upr.edu>
2023-09-27 22:47:58 -0400

    Added 'title' and 'paragraph' to styles

--- TutoringApp/components/atoms/Department.jsx
+++ TutoringApp/components/atoms/Department.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../constants/theme";
 import { Card, Title, Paragraph } from "react-native-paper";
 import Course from "../molecules/Course";
 
 const Department = ({ departmentName, courseData }) => {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggleExpand = () => {
-    setCollapsed(!collapsed);
-  };
-
   return (
-    <Card style={deparmentStyles.courseCard}>
+    <Card style={styles.courseCard}>
       <Card.Content>
-        <Title style={{ color: COLORS.text, fontSize: SIZES.xxLarge }}>
-          {departmentName}
-        </Title>
-        <Paragraph style={{ color: COLORS.text, fontSize: SIZES.large }}>
+        <Title style={styles.title}>{departmentName}</Title>
+        <Paragraph style={styles.paragraph}>
           Select a course to see its available tutors and schedules.
         </Paragraph>
         {courseData.map((data, index) => (
@@ -28,12 +20,19 @@ const Department = ({ departmentName, courseData }) => {
   );
 };
 
-const deparmentStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   courseCard: {
     marginVertical: 30,
     borderWidth: 2,
     height: "auto",
     borderColor: COLORS.primary,
+    color: COLORS.text
+  },
+  title: {
+    fontSize: SIZES.xxLarge,
+  },
+  paragraph: {
+    fontSize: SIZES.large,
   },
 });
 
