871ac7d475c6fe94593870834633ccd2f60b3da7
Jonathan-Rod <jonathan.rodriguez72@upr.edu>
2023-10-02 23:51:49 -0400

    Refactored Department component, added departmentMessage prop, and updated documentation

--- TutoringApp/components/atoms/Department.jsx
+++ TutoringApp/components/atoms/Department.jsx
@@ -10,17 +10,19 @@ import Course from "../molecules/Course";
  * @param {Object} props - The properties for the department component.
  * @param {string} props.departmentName - The name of the department.
  * @param {Array} props.courseData - The data of courses within the department.
+ * @param {string} props.departmentMessage - The optional message for the department.
  * @returns {JSX.Element} A Card component representing the department with its courses.
  */
-const Department = ({ departmentName, courseData }) => {
-  const departmentMessage = "Select a course to see its available tutors and schedules."
+const Department = ({
+  departmentName,
+  courseData,
+  departmentMessage = "Select a course to see its available tutors and schedules.",
+}) => {
   return (
     <Card style={styles.courseCard}>
       <Card.Content>
         <Title style={styles.title}>{departmentName}</Title>
-        <Paragraph style={styles.paragraph}>
-          {departmentMessage}
-        </Paragraph>
+        <Paragraph style={styles.paragraph}>{departmentMessage}</Paragraph>
         {courseData.map((data, index) => (
           <Course key={index} CourseName={data.name} Tutors={data.tutors} />
         ))}
