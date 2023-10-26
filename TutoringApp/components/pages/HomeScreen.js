aaa64d279bb8c92cd75f6ec8ea2109ee9f34b3a1
jvstinoo <justin.balbuena@upr.edu>
2023-10-26 17:39:09 -0400

    Querying user's active courses from database.

--- TutoringApp/components/pages/HomeScreen.js
+++ TutoringApp/components/pages/HomeScreen.js
@@ -1,125 +1,144 @@
 import React, { useState, useEffect } from "react";
 
 import {
-    Image,
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    FlatList,
-    TouchableOpacity,
-    ScrollView,
-    SafeAreaView
-    
-  } from "react-native";
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView
+
+} from "react-native";
 
 import Slider from '../organisms/Scroll';
 import CourseCard from '../atoms/CourseCard';
 import PastActiveCourseButton from "../atoms/PastActiveCourseButton";
 import { useNavigation } from "@react-navigation/native";
+import { fetchActiveCourses } from "../../config/supabaseClient";
 
 export default function HomeScreen() {
-    // let tut = [{'id': 0,
-    //     properties: {'name': "Joe Biden",
-    //     'specialty': "Economics",
-    //     'courses' : ['Intro to Economics', 'Finance'],
-    //     'rating': 4
-    //     }},
-    //     {id: 1, properties: {'name' : "Vannesa Ramos",
-    //     'specialty' : "Computer Science",
-    //     'courses' : ['CIIC3081', 'CIIC4020'],
-    //     'profile' : '',
-    //     'rating' : 3.7}}
-    // ];
-    //  Current Issue to look into, the console is complaining
-    //  that the tutor does not have a unique key, however, I believe
-    //  this issue should be solved when the data base is set.
+  // let tut = [{'id': 0,b
+  //     properties: {'name': "Joe Biden",
+  //     'specialty': "Economics",
+  //     'courses' : ['Intro to Economics', 'Finance'],
+  //     'rating': 4
+  //     }},
+  //     {id: 1, properties: {'name' : "Vannesa Ramos",
+  //     'specialty' : "Computer Science",
+  //     'courses' : ['CIIC3081', 'CIIC4020'],
+  //     'profile' : '',
+  //     'rating' : 3.7}}
+  // ];
+  //  Current Issue to look into, the console is complaining
+  //  that the tutor does not have a unique key, however, I believe
+  //  this issue should be solved when the data base is set.
 
-    const [status, setStatus] = useState("Active");
+  const [status, setStatus] = useState("Active");
 
-    const toggleStatus = () => {
-        if (status === "Active") {
-          setStatus("Past");
-        } else {
-          setStatus("Active");
-        }
-      };
-    
-    // Static Testing
-    const tutor = {
-        'name' : "Barack Obama",
-        'specialty' : "Economics",
-        'courses' : ['Intro to Economics', 'Finance'],
-        'profile' : '',
-        'rating' : 4
-      };
-      const tutor2 = {
-        'name' : "Trump",
-        'specialty' : "Computer Science",
-        'courses' : ['CIIC3081', 'CIIC4020'],
-        'profile' : '',
-        'rating' : 3.7
-      };
-    const tutor3 = {
-        'name' : "Jose River",
-        'specialty' : "English",
-        'courses' : ['INGL3010', 'INGL5030'],
-        'profile' : '',
-        'rating' : 4.7
-      };
-    const tutors2 = [tutor, tutor2, tutor3]
+  const toggleStatus = () => {
+    if (status === "Active") {
+      setStatus("Past");
+    } else {
+      setStatus("Active");
+    }
+  };
 
-    const test_course_card_active = [{courseImage: require('../../assets/data-structures.png'), courseName: 'Data Structures', courseTutor: 'Jose', link: 'www.google.com', startDate: '9/25/2023', endDate: '10/20/2024', startTime: '1:00pm', endTime: '2:00pm', tutor: 'Jose'},{courseImage: require('../../assets/electric.jpeg'), courseName: 'Electric', courseTutor: 'Pablo',link: 'www.google.com', startDate: '9/25/2023', endDate: '10/20/2024', startTime: '1:00pm', endTime: '2:00pm', tutor: 'Paco'}]
-    const test_course_card_past = [{courseImage: require('../../assets/electric.jpeg'), courseName: 'Data Structures', courseTutor: 'Paco'}, {courseImage: require('../../assets/electric.jpeg'), courseName: 'Electric', courseTutor: 'Pablo'}]
-    const courseData = status === "Active" ? test_course_card_active : test_course_card_past;
+  // Static Testing
+  const tutor = {
+    'name': "Barack Obama",
+    'specialty': "Economics",
+    'courses': ['Intro to Economics', 'Finance'],
+    'profile': '',
+    'rating': 4
+  };
+  const tutor2 = {
+    'name': "Trump",
+    'specialty': "Computer Science",
+    'courses': ['CIIC3081', 'CIIC4020'],
+    'profile': '',
+    'rating': 3.7
+  };
+  const tutor3 = {
+    'name': "Jose River",
+    'specialty': "English",
+    'courses': ['INGL3010', 'INGL5030'],
+    'profile': '',
+    'rating': 4.7
+  };
+  const tutors2 = [tutor, tutor2, tutor3]
 
+  const activeCourses = fetchActiveCourses('802201628');
+  //const test_course_card_active = [{ courseImage: require('../../assets/data-structures.png'), courseName: 'Data Structures', courseTutor: 'Jose', link: 'www.google.com', startDate: '9/25/2023', endDate: '10/20/2024', startTime: '1:00pm', endTime: '2:00pm', tutor: 'Jose' }, { courseImage: require('../../assets/electric.jpeg'), courseName: 'Electric', courseTutor: 'Pablo', link: 'www.google.com', startDate: '9/25/2023', endDate: '10/20/2024', startTime: '1:00pm', endTime: '2:00pm', tutor: 'Paco' }]
+  const active_courses = []
 
-    return (
-        <ScrollView style={styles.container}>
-            <View style={styles.scroll_box}>
-                <View style={styles.spacer} />
-                <View style={styles.toggle_container_with_header}>
-                    <Text style={styles.headingText}>{status === "Active" ? "Active" : "Past"} Courses</Text>
-                    <PastActiveCourseButton onPress={toggleStatus} status={status}/>
-                </View>
-                <Slider components={courseData} isCourseCard={true} />
-                <View style={styles.spacer} />
-                <Text style={styles.headingText}>Recommended Tutors</Text>
-                <Slider components={tutors2} isRecommendedCard={true} />
-                <View style={styles.spacer} />
-            </View>  
-        </ScrollView>
-      );
+  if (activeCourses != null) {
+    for (var i = 0; i < activeCourses.length && i < 10; i++) {
+      const course = {
+        courseImage: require('../../assets/data-structures.png'),
+        courseName: String(activeCourses[i].course),
+        courseTutor: String(activeCourses[i].tutor_name),
+        link: 'www.google.com',
+        startDate: '9/25/2023',
+        endDate: '10/20/2024',
+        startTime: '1:00pm',
+        endTime: '2:00pm',
+        tutor: 'Jose'
+      }
+      active_courses.push(course)
+    }
+  }
+  const test_course_card_past = [{ courseImage: require('../../assets/electric.jpeg'), courseName: 'Data Structures', courseTutor: 'Paco' }, { courseImage: require('../../assets/electric.jpeg'), courseName: 'Electric', courseTutor: 'Pablo' }]
+  const courseData = status === "Active" ? active_courses : test_course_card_past;
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.scroll_box}>
+        <View style={styles.spacer} />
+        <View style={styles.toggle_container_with_header}>
+          <Text style={styles.headingText}>{status === "Active" ? "Active" : "Past"} Courses</Text>
+          <PastActiveCourseButton onPress={toggleStatus} status={status} />
+        </View>
+        <Slider components={courseData} isCourseCard={true} />
+        <View style={styles.spacer} />
+        <Text style={styles.headingText}>Recommended Tutors</Text>
+        <Slider components={tutors2} isRecommendedCard={true} />
+        <View style={styles.spacer} />
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    scroll_box: {
-        padding: 5
-    },
-    box: {
-        width: '100%',
-        height: 180,
-    },
-    inner: {
-        flex:1,
-        backgroundColor: '#eee',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    spacer: {
-        height: 40,
-        width: '100%'
-    },
-    headingText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        paddingHorizontal: 8
-    },
-    toggle_container_with_header: {
-        flexDirection: 'row',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  scroll_box: {
+    padding: 5
+  },
+  box: {
+    width: '100%',
+    height: 180,
+  },
+  inner: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  spacer: {
+    height: 40,
+    width: '100%'
+  },
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8
+  },
+  toggle_container_with_header: {
+    flexDirection: 'row',
+  }
 });
--- TutoringApp/config/supabaseClient.js
+++ TutoringApp/config/supabaseClient.js
@@ -1,8 +1,34 @@
 import { createClient } from '@supabase/supabase-js'
+import React, { useState, useEffect } from "react";
 
 const supabaseUrl = "https://qunwadbiwbycvmgscyyg.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1bndhZGJpd2J5Y3ZtZ3NjeXlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUzNDAxNjcsImV4cCI6MjAxMDkxNjE2N30.nlr1UhwV5dmZKh2uoCWV0mTeKAdGAznspCqk5oBxQYs"
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+export const fetchActiveCourses = (user_id) => {
+    const [fetchError, setFetchError] = useState(null)
+    const [activeCourses, setActiveCourses] = useState(null)
+
+    useEffect(() => {
+        const fetchActiveCourses = async () => {
+            const { data, error } = await supabase.rpc('get_active_courses', { student: user_id })
+
+            if (error) {
+                setFetchError('Could not fetch active courses')
+                setActiveCourses(null)
+                console.log(error)
+            }
+
+            if (data) {
+                setActiveCourses(data)
+                setFetchError(null)
+            }
+        }
+        fetchActiveCourses()
+    }, [])
+    return activeCourses;
+}
 
 export default supabase
